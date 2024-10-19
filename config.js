1a3809f5e6d7acc56f9f1d9daf520e3641d1b099
GOD-SETH <158117163+MRFGS@users.noreply.github.com>
2024-10-19 16:06:44 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,24 @@
+{
+  "inbounds": [{
+    "port": 443,
+    "protocol": "vless",
+    "settings": {
+      "clients": [{
+        "id": "2a5ee225-e354-4fd5-bfb0-2ff78e218907", 
+        "level": 0,
+        "email": "exap7149@gmail.com"
+      }],
+      "decryption": "none"
+    },
+    "streamSettings": {
+      "network": "ws",
+      "wsSettings": {
+        "path": "/vless"
+      }
+    }
+  }],
+  "outbounds": [{
+    "protocol": "freedom",
+    "settings": {}
+  }]
+}
